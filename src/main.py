9c6f4dc62c44ad9ceb7a2020143160b9887bb9aa
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-10-18 20:56:35 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import torch
 import subprocess
 import sys
 
@@ -16,15 +15,14 @@ install("accelerate==0.34.2")
 install("huggingface_hub==0.25.1")
 install("unsloth[cu121-ampere-torch240] @ git+https://github.com/unslothai/unsloth.git")
 
-subprocess.run([sys.executable, "train.py"], check=True)
-
-subprocess.run([sys.executable, "inference.py"], check=True)
-
-subprocess.run([sys.executable, "convert.py"], check=True)
-
+import torch
 
 if torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8:
     install("packaging")
     install("ninja")
     install("einops")
     install("flash-attn>=2.6.3")
+
+subprocess.run([sys.executable, "train.py"], check=True)
+subprocess.run([sys.executable, "inference.py"], check=True)
+subprocess.run([sys.executable, "convert.py"], check=True)
