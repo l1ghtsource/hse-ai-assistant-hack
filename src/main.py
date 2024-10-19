893f770fd63d8a2665c13ed4a5f6fa59bb907e73
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-10-19 19:24:45 +0300

    add flag

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import subprocess
 import sys
 
+train = False # установить True, если необходимо обучить модель с нуля
+
 subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
-subprocess.run([sys.executable, "train.py"], check=True)
+
+if train:
+  subprocess.run([sys.executable, "train.py"], check=True)
+  
 subprocess.run([sys.executable, "inference.py"], check=True)
 subprocess.run([sys.executable, "convert.py"], check=True)
